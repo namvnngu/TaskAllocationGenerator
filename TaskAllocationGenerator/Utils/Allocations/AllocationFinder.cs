14ffea2614e7cfa725cb10ad1642106bff592ced
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-05-18 01:49:52 +1000

    feat: add allocation finder

---
+++ TaskAllocationGenerator/Utils/Allocations/AllocationFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskAllocationUtils.Files;
+
+namespace TaskAllocationGenerator.Utils.Allocations
+{
+    public class AllocationFinder
+    {
+        public ConfigurationFile Configuration { get; set; }
+
+        public AllocationFinder()
+        {
+
+        }
+
+        public AllocationFinder(ConfigurationFile configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void Run()
+        {
+
+        }
+    }
+}
